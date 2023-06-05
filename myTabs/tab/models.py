e59f3a2bb985ab1783dab206667c9c7df59489ef
j-e-kroczek <j.e.kroczek@gmail.com>
2023-06-05 12:22:23 +0200

    Updated myTabs page
    Added:
    - expenses table
    - balance bars

--- myTabs/tab/models.py
+++ myTabs/tab/models.py
@@ -3,39 +3,51 @@ from django.contrib.auth.models import User
 from django.db.models import Q, CheckConstraint
 from datetime import datetime
 
+
 class Tab(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f'{self.name} - {self.id}'
 
-    
+    def __str__(self):
+        return f"{self.name} - {self.id}"
+
+
 class Belonging(models.Model):
     class Meta:
-        unique_together = (('user', 'tab'),)
-        
+        unique_together = (("user", "tab"),)
+
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tab = models.ForeignKey(Tab, on_delete=models.CASCADE)
     joined_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
-        return f'{self.user.username} - {self.tab}'
+        return f"{self.user.username} - {self.tab}"
+
 
 class ExpenseType(models.Model):
     class Meta:
-        constraints = [CheckConstraint(check=(Q(is_private=False)&Q(tab__isnull=True)|(Q(is_private=True)&Q(tab__isnull=False))), name='is_private_no_tab')]
-        unique_together = (('name', 'tab'))
+        constraints = [
+            CheckConstraint(
+                check=(
+                    Q(is_private=False) & Q(tab__isnull=True)
+                    | (Q(is_private=True) & Q(tab__isnull=False))
+                ),
+                name="is_private_no_tab",
+            )
+        ]
+        unique_together = ("name", "tab")
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     is_private = models.BooleanField(default=True)
     tab = models.ForeignKey(Tab, on_delete=models.CASCADE, null=True, blank=True)
-    
+
     def __str__(self):
-        return f'{self.name} - {self.id} - {self.tab}'
+        return f"{self.name} - {self.id} - {self.tab}"
+
 
 class Expense(models.Model):
     id = models.AutoField(primary_key=True)
@@ -47,16 +59,17 @@ class Expense(models.Model):
     date = models.DateTimeField(default=datetime.today(), editable=True)
 
     def __str__(self):
-        return f'{self.name} - {self.id}'
-    
+        return f"{self.name} - {self.id}"
+
+
 class Associating(models.Model):
     class Meta:
-        unique_together = (('user','expense'))
+        unique_together = ("user", "expense")
+
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
     cost = models.DecimalField(max_digits=8, decimal_places=2)
-    
+
     def __str__(self):
-        return f'{self.expense} - {self.user} - {self.id}'
-    
+        return f"{self.expense} - {self.user} - {self.id} ({self.cost})"
--- myTabs/tab/utils.py
+++ myTabs/tab/utils.py
@@ -1,4 +1,8 @@
-from tab.models import Belonging
+from tab.models import Belonging, Expense
+from collections import defaultdict
+from tab.models import Tab, Associating, Belonging
+import sys
+import random
 
 
 def get_user_tabs(user):
@@ -6,3 +10,128 @@ def get_user_tabs(user):
     for belonging in Belonging.objects.filter(user=user):
         user_tabs.append(belonging.tab)
     return user_tabs
+
+
+def get_tab_users(tab):
+    tab_users = []
+    for belonging in Belonging.objects.filter(tab=tab):
+        tab_users.append(belonging.user)
+    return tab_users
+
+
+def get_user_balance_in_tab(user, tab):
+    if user in get_tab_users(tab):
+        for expense in Expense.objects.filter(tab=tab):
+            if expense.user == user:
+                return expense.amount
+
+
+def get_debts(tab):
+    debts = []
+    for assosiation in Associating.objects.filter(expense__tab=tab):
+        if assosiation.user != assosiation.expense.buyer:
+            debts.append(
+                (assosiation.user, assosiation.expense.buyer, float(assosiation.cost))
+            )
+    return debts
+
+
+def compute_balances(debts, people):
+    balances = {person: 0 for person in people}
+    for debtor, creditor, value in debts:
+        balances[debtor] -= value
+        balances[creditor] += value
+    for person, amount in balances.items():
+        balances[person] = round(amount, 2)
+    return balances
+
+
+def get_procent_balances(balances):
+    abs_balances = {person: abs(amount) for person, amount in balances.items()}
+    result = []
+    max_balance = max(abs_balances.values())
+    for person, amount in balances.items():
+        result.append((person, amount, int(round(abs(amount) / max_balance, 2) * 100)))
+    return result
+
+
+def simplify_minflow(debts, people):
+    balances = compute_balances(debts, people)
+    transactions = []
+    debtors = {p: b for (p, b) in balances.items() if b < 0}
+    creditors = {p: b for (p, b) in balances.items() if b > 0}
+
+    while debtors:
+        (debtor, debt) = next(iter(debtors.items()))
+        (creditor, credit) = next(iter(creditors.items()))
+        amount = min(-debt, credit)
+        transactions.append((debtor, creditor, amount))
+
+        creditors[creditor] -= amount
+        debtors[debtor] += amount
+        if creditors[creditor] == 0:
+            del creditors[creditor]
+        if debtors[debtor] == 0:
+            del debtors[debtor]
+
+    return transactions
+
+
+def simplify_with_collector(balances):
+    collector = next(iter(balances.keys()))
+    return [
+        (collector, person, balance)
+        for (person, balance) in balances.items()
+        if person != collector
+    ]
+
+
+def show_transactions(transactions):
+    for debtor, creditor, value in transactions:
+        if value > 0:
+            print(f"{debtor} owes {creditor} ${value}")
+        else:
+            print(f"{creditor} owes {debtor} ${-value}")
+
+
+import itertools
+
+
+def find_zero_subset(balances):
+    for i in range(1, len(balances)):
+        for subset in itertools.combinations(balances.items(), i):
+            if sum([balance[1] for balance in subset]) == 0:
+                return [balance[0] for balance in subset]
+    return None
+
+
+def run_opt(debts, people):
+    remaining_set = compute_balances(debts, people)
+    subsets = []
+    while (subset := find_zero_subset(remaining_set)) is not None:
+        subsets.append(subset)
+        remaining_set = {
+            x[0]: x[1] for x in remaining_set.items() if x[0] not in subset
+        }
+    subsets.append(list(remaining_set.keys()))
+    balances = compute_balances(debts, people)
+    optimal_transactions = []
+    for subset in subsets:
+        subset_balances = {person: balances[person] for person in subset}
+        optimal_transactions.extend(simplify_with_collector(subset_balances))
+    final_optimal_transactions = []
+    for transaction in optimal_transactions:
+        if transaction[2] < 0.0:
+            new_transaction = (transaction[1], transaction[0], -transaction[2])
+            final_optimal_transactions.append(new_transaction)
+        else:
+            final_optimal_transactions.append(transaction)
+    return final_optimal_transactions
+
+
+def get_tab_expenses(tab):
+    result = {}
+    expenses = Expense.objects.filter(tab=tab)
+    for expense in expenses:
+        result[expense] = Associating.objects.filter(expense=expense)
+    return result
